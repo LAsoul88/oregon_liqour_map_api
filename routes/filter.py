83e2bfcf5528df2ed052547e6f281b046c5507cd
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2022-11-12 14:17:46 -0800

    added all filters

--- routes/filter.py
+++ routes/filter.py
@@ -23,22 +23,51 @@ def filter_results(request):
     for store in stores:
       results.append(store.serialized)
     return results
-  elif filter_string == 'Browse All Locations':
+  else: 
+    if filter_string == 'Browse All Liqour':
+      filter_string = 'description'
+    attribute = getattr(Liqour, filter_string.replace(' ', '_').lower())
+    liqour = Liqour.query.filter(attribute.ilike("%{}%".format(search_string))).all()
+    for bottle in liqour:
+      results.append(bottle.serialized)
     return results
-  elif filter_string == 'Type':
-    return results
-  elif filter_string == 'Case Price':
-    return results
-  elif filter_string == 'Bottle Price':
-    return results
-  elif filter_string == 'size': 
-    return results
-  elif filter_string == 'proof':
-    return results
-  elif filter_string == 'age':
-    return results
+  # elif filter_string == 'Browse All Liqour':
+  #   liqour = Liqour.query.filter(Liqour.description.ilike("%{}%".format(search_string))).all()
+  #   for bottle in liqour:
+  #     results.append(bottle.serialized)
+  #   return results
+  # elif filter_string == 'Type':
+  #   liqour = Liqour.query.filter(Liqour.type.ilike("%{}%".format(search_string))).all()
+  #   for bottle in liqour:
+  #     results.append(bottle.serialized)
+  #   return results
+  # elif filter_string == 'Case Price':
+  #   liqour = Liqour.query.filter(Liqour.case_price.ilike("%{}%".format(search_string))).all()
+  #   for bottle in liqour:
+  #     results.append(bottle.serialized)
+  #   return results
+  # elif filter_string == 'Bottle Price':
+  #   liqour = Liqour.query.filter(Liqour.bottle_price.ilike("%{}%".format(search_string))).all()
+  #   for bottle in liqour:
+  #     results.append(bottle.serialized)
+  #   return results
+  # elif filter_string == 'size': 
+  #   liqour = Liqour.query.filter(Liqour.size.ilike("%{}%".format(search_string))).all()
+  #   for bottle in liqour:
+  #     results.append(bottle.serialized)
+  #   return results
+  # elif filter_string == 'proof':
+  #   liqour = Liqour.query.filter(Liqour.proof.ilike("%{}%".format(search_string))).all()
+  #   for bottle in liqour:
+  #     results.append(bottle.serialized)
+  #   return results
+  # elif filter_string == 'age':
+  #   liqour = Liqour.query.filter(Liqour.age.ilike("%{}%".format(search_string))).all()
+  #   for bottle in liqour:
+  #     results.append(bottle.serialized)
+  #   return results
 # (Browse By Store) - Address
-# (Browse All Locations) - Description
+# (Browse All Liqour) - Description
 # (Type) - Category
 # (Case Price) - Case Price
 # (Bottle Price) - Bottle Price
