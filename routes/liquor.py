d06ad2bb06ff0c5ad1ca9e01e837e03edb1e9169
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2022-11-30 13:05:54 -0700

    added error handling for liquor and store id routes

--- routes/liquor.py
+++ routes/liquor.py
@@ -14,12 +14,23 @@ def get_bottles():
 
 # get single bottle
 def get_bottle(id):
-  bottle = Liquor.query.filter_by(id = id).one()
-  liquor_store_table = LiquorStore.query.filter_by(liquor_id = id).order_by(LiquorStore.store_id.asc()).all()
-  store_list = []
-  for row in liquor_store_table:
-    store = Store.query.filter_by(id = row.store_id).one()
-    formatted_store = format_store(store)
-    formatted_store['quantity'] = row.quantity
-    store_list.append(formatted_store)
-  return format_liquor(bottle, store_list)
+  try:
+    print('we got here')
+    bottle = Liquor.query.filter_by(id = id).one()
+    liquor_store_table = LiquorStore.query.filter_by(liquor_id = id).order_by(LiquorStore.store_id.asc()).all()
+    store_list = []
+    for row in liquor_store_table:
+      store = Store.query.filter_by(id = row.store_id).one()
+      formatted_store = format_store(store)
+      formatted_store['quantity'] = row.quantity
+      store_list.append(formatted_store)
+    return format_liquor(bottle, store_list)
+  except:
+    bottle = Liquor.query.filter_by(id = id).scalar()
+    liquor_store_table = LiquorStore.query.filter_by(liquor_id = id).order_by(LiquorStore.store_id.asc()).scalar()
+    if not bottle:
+      print(f'== did not find liquor id: {id} ==')
+    elif not liquor_store_table:
+      print(f'== did not find liquor_store_table associated with liquor id: {id}')
+    else:
+      print('== something else went wrong ==')
