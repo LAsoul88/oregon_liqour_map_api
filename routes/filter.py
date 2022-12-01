d3374e41ac275e603b4ef32d85cd9cc9a182a3d3
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2022-12-01 14:30:03 -0700

    modified filter route to allow for multiple filters

--- routes/filter.py
+++ routes/filter.py
@@ -4,57 +4,62 @@ from models.liquor import Liquor
 # filter object
 # {
 #   data: {
-#     search: <query here>,
-#     filter: <filter here>,
-#     range: <range here>,
+#     filters: [
+#       {
+#         search: <query here>,
+#         filter: <filter here>,
+#         range: <range here>,
+#       },
+#     ]
 #   }
 # }
 
 def filter_results(request):
   results = []
-  filter_string = request['data']['filter']
-  search_string = request['data']['search']
-  
-  if filter_string == 'Browse By Store':
+  filter_list = request['data']['filters']
+
+  if filter_list[0]['filter'] == 'Browse By Store':
+    search_string = filter_list[0]['search']
+    print(search_string)
     stores = Store.query.filter(Store.address.ilike("%{}%".format(search_string))).all()
     for store in stores:
       results.append(store.serialized)
-    return results
+    return results 
 
-  elif 'Price' in filter_string:
-    range_string = request['data']['range']
-    search_price = float(search_string.replace('$', ''))
-    price = getattr(Liquor, filter_string.replace(' ', '_').lower())
-    liquor = []
-    if range_string == 'lower':
-      liquor = Liquor.query.filter(price <= search_price).order_by(price.asc()).all()
-    elif range_string == 'higher':
-      liquor = Liquor.query.filter(price >= search_price).order_by(price.asc()).all()
-    for bottle in liquor:
-      results.append(bottle.serialized)
-    return results
-    
-  elif filter_string == 'Proof':
-    range_string = request['data']['range']
-    proof = float(search_string)
-    liquor = []
-    if range_string == 'lower':
-      liquor = liquor.query.filter(Liquor.proof <= proof).order_by(Liquor.proof.asc()).all()
-    elif range_string == 'higher':
-      liquor = Liquor.query.filter(Liquor.proof >= proof).order_by(Liquor.proof.asc()).all()
-    for bottle in liquor:
-      results.append(bottle.serialized)
-    return results
+  query = Liquor.query
+  
+  for field in filter_list:
+    filter_string = field['filter']
+    search_string = field['search']    
 
-  else: 
-    if filter_string == 'Browse All Liquor':
-      filter_string = 'description'
-    descriptor = getattr(Liquor, filter_string.replace(' ', '_').lower())
-    liquor = Liquor.query.filter(descriptor.ilike("%{}%".format(search_string))).all()
-    for bottle in liquor:
-      results.append(bottle.serialized)
-    return results
+    if 'Price' in filter_string:
+      range_string = field['range']
+      search_price = float(search_string.replace('$', ''))
+      price = getattr(Liquor, filter_string.replace(' ', '_').lower())
+      if range_string == 'lower':
+        query = query.filter(price <= search_price).order_by(price.asc())
+      elif range_string == 'higher':
+        query = query.filter(price >= search_price).order_by(price.asc())
     
+    elif filter_string == 'Proof':
+      range_string = field['range']
+      proof = float(search_string)
+      if range_string == 'lower':
+        query = query.filter(Liquor.proof <= proof)
+      elif range_string == 'higher':
+        query = query.filter(Liquor.proof >= proof)
+    
+    else: 
+      if filter_string == 'Browse All Liquor':
+        filter_string = 'description'
+      descriptor = getattr(Liquor, filter_string.replace(' ', '_').lower())
+      query = query.filter(descriptor.ilike("%{}%".format(search_string)))
+
+  liquor = query.all()
+  for bottle in liquor:
+    results.append(bottle.serialized)
+  return results
+
 # (Browse By Store) - Address
 # (Browse All Liquor) - Description
 # (Type) - Category
